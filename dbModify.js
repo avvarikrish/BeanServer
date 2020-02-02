c10e93eb6d34b3d9f283eb1675274e122180394a
archit-garg12 <gargkarchit@gmail.com>
2020-02-02 12:39:12 -0800

    bb

--- dbModify.js
+++ dbModify.js
@@ -131,7 +131,7 @@ exports.sendToken = function(req, res)
 exports.getParticipants = function(req, res)
 {
     client.video.rooms(req.query.roomName).participants
-        .each({status:"complete"}, (part) => console.log(part))
+        .each({status:"complete"}, (part) => {console.log(part.sid)})
 }
 
 exports.getRecording = function(req, res)
